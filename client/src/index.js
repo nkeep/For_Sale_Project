667463426ec03bfea3e847a8db6c0a0eb4edc8fe
Nathan Keep <ntkeep@gmail.com>
2021-09-26 14:19:59 -0500

    centering the game

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import Game from "./scenes/game";
 import Home from "./scenes/home";
 
 const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 1280,
-  height: 780,
-  scene: [Home, Game],
-  dom: { createContainer: true },
-  loader: { crossOrigin: true },
+	type: Phaser.AUTO,
+	parent: "phaser-example",
+	width: 1280,
+	height: 780,
+	scene: [Home, Game],
+	dom: { createContainer: true },
+	loader: { crossOrigin: true },
+	autoCenter: true,
 };
 
 const game = new Phaser.Game(config);
